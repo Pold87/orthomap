7a5bc7d6680ce29747b64060f0868d510961bdc0
Volker Strobel <volker.strobel87@gmail.com>
2015-11-12 12:03:35 +0100

    First commit

---
+++ save_images.py
@@ -0,0 +1,21 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(1)
+
+pic_num = 0
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Display the resulting frame
+    cv2.imwrite('imgs/' + str(pic_num) + '.png', frame)
+    if cv2.waitKey(500) & 0xFF == ord('q'):
+        break
+
+    pic_num += 1
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
